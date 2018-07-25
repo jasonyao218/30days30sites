c47390074dec961a27ad47900fa48d95bc41e286
Jiachen Yao <mezi@bu.edu>
2018-07-25 15:58:37 -0400

    Update features to nav bar, add other pages.

--- Events/js/nav.js
+++ Events/js/nav.js
@@ -1,8 +1,10 @@
-$(window).on('scroll', function() {
-	if($(window).scrollTop()){
-		$('nav').addClass('black');
-	}
-	else{
-		$('nav').removeClass('black');
-	}
-})
+$(document).ready(function(){
+	$(window).on('scroll', function() {
+		if($(window).scrollTop()){
+			$('nav').addClass('black');
+		}
+		else{
+			$('nav').removeClass('black');
+		}
+	})
+});
