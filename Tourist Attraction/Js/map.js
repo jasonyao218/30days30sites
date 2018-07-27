441a45d0ccf0065dddd43a286dfbdeb531e84555
Jiachen Yao <mezi@bu.edu>
2018-07-27 15:47:36 -0400

    Update place, weather, and info section.

---
+++ Tourist Attraction/Js/map.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	var map;
+    function initMap() {
+       	map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+});
